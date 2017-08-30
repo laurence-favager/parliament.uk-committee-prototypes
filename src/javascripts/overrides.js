a3c74c07780dbab30062d26300210290c59cb3b6
James Vinter <vinterj@Jamess-MacBook-Pro.local>
2017-08-30 16:54:11 +0100

    Updates to CSS overrides and form logic

--- src/javascripts/overrides.js
+++ src/javascripts/overrides.js
@@ -1,3 +1,16 @@
 (function () {
-  console.log('Override javascript executed!');
-})();
+  var checkbox = document.getElementById('chkAgent');
+  var container = document.getElementById('detAgent');
+  var fn = function () {
+    if (checkbox.checked) {
+      container.style.display = 'block';
+    } else {
+      container.style.display = 'none';
+    }
+  };
+
+  checkbox.onclick = fn;
+
+  fn.apply(checkbox);
+
+}());
