c7dffff6cf6f094193a72052957f477f79daa396
James Vinter <vinterj@parliament.uk>
2017-09-03 20:26:15 +0100

    Template content changes, CSS & JS overide updates

--- src/javascripts/overrides.js
+++ src/javascripts/overrides.js
@@ -1,16 +1,42 @@
+// Function to show/hide content when checkbox clicked
 (function () {
-  var checkbox = document.getElementById('chkAgent');
-  var container = document.getElementById('detAgent');
-  var fn = function () {
-    if (checkbox.checked) {
-      container.style.display = 'block';
-    } else {
-      container.style.display = 'none';
+  if (document.getElementById('toggleContent')) {
+    var checkbox = document.getElementById('toggleContent');
+    var container = document.getElementById('contentPane');
+    var fnsh = function () {
+      if (checkbox.checked) {
+        container.style.display = 'block';
+      } else {
+        container.style.display = 'none';
+      }
+    };
+
+    checkbox.onclick = fnsh;
+
+    fnsh.apply(checkbox);
+  }
+}());
+
+// Function to listen for checked radio buttons and change form action
+(function () {
+  if (document.querySelectorAll('input[type=radio]')) {
+
+    var radios = document.querySelectorAll('input[type=radio]');
+
+    function getCheckedRadioIndex(radios) {
+      for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+          var r = radios[i].dataset.route;
+          return r;
+        }
+      }
     }
-  };
 
-  checkbox.onclick = fn;
-
-  fn.apply(checkbox);
-
+    for (var i = 0; i < radios.length; i++) {
+      radios[i].onclick = function(event) {
+        var route = getCheckedRadioIndex(radios);
+        document.getElementById('submitterType').action=route;
+      };
+    }
+  }
 }());
