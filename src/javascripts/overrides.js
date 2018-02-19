99372f0a0e791123c9325d56877b8240958a9e5c
James Vinter <>
2018-02-19 13:29:15 +0000

    Updates to hybrid bill templates and site wide javascript

--- src/javascripts/overrides.js
+++ src/javascripts/overrides.js
@@ -1,3 +1,13 @@
+// Polyfill to enable 'forEach' in non ES6 supported browser
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
 // JavaScript enabled message function
 UK_Parliament.jsEnabled = function () {
   if (document.querySelector('body.has-js')) {
@@ -115,16 +125,19 @@ UK_Parliament.enableSubmit = function () {
             var inputCheckName = element.name;
             var inputCheckNamesArray = document.getElementsByName(inputCheckName);
 
+            // function to set submit button type from radio selection
+            var styleSetRadio = function () {
+              if (this.dataset.submit == 'disabled' && this.checked) {
+                styleDisable(userButton);
+              } else {
+                styleEnable(userButton, userButtonClass);
+              }
+            };
+
             // Loop radio group and grab clicked radio button
-            inputCheckNamesArray.forEach(function (radios, index) {
-              radios.addEventListener('click', function () {
-                if (this.dataset.submit == 'disabled' && this.checked) {
-                  styleDisable(userButton);
-                } else {
-                  styleEnable(userButton, userButtonClass);
-                }
-              });
-            });
+            for (var x = 0; x < inputCheckNamesArray.length; x++) {
+              inputCheckNamesArray[x].addEventListener('click', styleSetRadio, false);
+            }
           }
         });
       }
@@ -187,19 +200,19 @@ UK_Parliament.countryPetitioner = function () {
     var indCntSl = document.getElementById('countrySelect');
     var indCntUk = document.getElementById('country-uk');
     var indCntRw = document.getElementById('country-row');
-    var indPcd = document.getElementById('postCodeInd');
+    //var indPcd = document.getElementById('postCodeInd');
 
     indCntSl.style.display = 'none';
-    indPcd.style.display = 'block';
+    //indPcd.style.display = 'block';
 
     indCntUk.addEventListener('click', function () {
       indCntSl.style.display = 'none';
-      indPcd.style.display = 'block';
+      //indPcd.style.display = 'block';
     });
 
     indCntRw.addEventListener('click', function () {
       indCntSl.style.display = 'block';
-      indPcd.style.display = 'none';
+      //indPcd.style.display = 'none';
     });
   }
 };
@@ -212,21 +225,130 @@ UK_Parliament.countryRepresentative = function () {
     var repCntSl = document.getElementById('countrySelectRep');
     var repCntUk = document.getElementById('country-uk-rep');
     var repCntRw = document.getElementById('country-row-rep');
-    var repPcd = document.getElementById('postCodeRep');
+    //var repPcd = document.getElementById('postCodeRep');
 
     repCntSl.style.display = 'none';
-    repPcd.style.display = 'block';
+    //repPcd.style.display = 'block';
 
     repCntUk.addEventListener('click', function () {
       repCntSl.style.display = 'none';
-      repPcd.style.display = 'block';
+      //repPcd.style.display = 'block';
     });
 
     repCntRw.addEventListener('click', function () {
       repCntSl.style.display = 'block';
-      repPcd.style.display = 'none';
+      //repPcd.style.display = 'none';
     });
   }
 };
 
 UK_Parliament.countryRepresentative();
+
+// Dropdown switch for hiding/showing content using form elements
+UK_Parliament.dropdownSwitch = function () {
+
+  if (document.querySelector('[data-dropdown="switch"]')) {
+
+    // Grab all elements which act as content switches
+    var dropdownSwitcherList = document.querySelectorAll('[data-dropdown="switch"]');
+
+    // Grab all elements which act as content content blocks
+    var dropdownContentList = document.querySelectorAll('[data-dropdown-content]');
+
+    // Find closest parent element function
+    var closestParentElement = function (el, fn) {
+      return el && (fn(el) ? el : closestParentElement(el.parentElement, fn));
+    };
+
+    // Show hidden content blocks function
+    var contentShow = function (dc) {
+      dc.style.display = 'block';
+    };
+
+    // Hide visible content blocks function
+    var contentHide = function (dc) {
+      dc.style.display = 'none';
+    };
+
+    // Set default content visibility state
+    dropdownContentList.forEach(function (elements, index) {
+
+      // Grab visibility state (show/hide) of each content block
+      var dropdownContentInitalState = elements.getAttribute('data-dropdown-content');
+
+      if (dropdownContentInitalState === 'show') {
+        contentShow(elements);
+      } else if (dropdownContentInitalState === 'hide') {
+        contentHide(elements);
+      }
+    });
+
+    // Loop through content switches
+    dropdownSwitcherList.forEach(function (elements, index) {
+
+      // Grab the switch/content holder using closestElement function
+      var dropdownHolder = closestParentElement(elements, function (el) {
+        return el.querySelector('[data-dropdown-content]');
+      });
+
+      // Grab the content block associated with holder and switch
+      var dropdownContent = dropdownHolder.querySelector('[data-dropdown-content]');
+
+      // Store reference to content data attribute
+      var dropdownContentState = dropdownContent.getAttribute('data-dropdown-content');
+
+      // Handle checkbox switches
+      if (elements.type === 'checkbox') {
+        elements.addEventListener('click', function () {
+          if (this.checked) {
+            if (dropdownContentState === 'show') {
+              contentHide(dropdownContent);
+            } else if (dropdownContentState === 'hide') {
+              contentShow(dropdownContent);
+            }
+          } else {
+            if (dropdownContentState === 'show') {
+              contentShow(dropdownContent);
+            } else if (dropdownContentState === 'hide') {
+              contentHide(dropdownContent);
+            }
+          }
+        });
+      }
+
+      // Handle radio group switches
+      if (elements.type === 'radio') {
+
+        // Grab radio group name
+        var inputSwitchName = elements.name;
+
+        // Grab radio group elements with that name
+        var inputSwitchNamesArray = document.getElementsByName(inputSwitchName);
+
+        // function to set submit button type from radio selection
+        var styleSetRadio = function () {
+          if (this.getAttribute('data-dropdown') === 'switch' && this.checked) {
+            if (dropdownContentState === 'show') {
+              contentHide(dropdownContent);
+            } else if (dropdownContentState === 'hide') {
+              contentShow(dropdownContent);
+            }
+          } else {
+            if (dropdownContentState === 'show') {
+              contentShow(dropdownContent);
+            } else if (dropdownContentState === 'hide') {
+              contentHide(dropdownContent);
+            }
+          }
+        };
+
+        // Loop radio group and grab clicked radio button
+        for (var x = 0; x < inputSwitchNamesArray.length; x++) {
+          inputSwitchNamesArray[x].addEventListener('click', styleSetRadio, false);
+        }
+      }
+    });
+  }
+};
+
+UK_Parliament.dropdownSwitch();
