32435f0ce6153b3aa78b843d98000e89ebcce498
James Vinter <>
2018-08-07 13:06:30 +0100

    Updates to committee templates and js overrides

--- src/javascripts/overrides.js
+++ src/javascripts/overrides.js
@@ -8,6 +8,11 @@ if (window.NodeList && !NodeList.prototype.forEach) {
   };
 }
 
+// Function to go back one page in browser window
+function goBack() {
+  window.history.back();
+}
+
 // Function to grab and store submitter type
 UK_Parliament.submitterIdentification = function () {
   if (document.querySelector('[data-evidence="form"]')) {
@@ -36,6 +41,8 @@ UK_Parliament.submitterIdentification = function () {
   }
 };
 
+UK_Parliament.submitterIdentification();
+
 // Function to route users correctly depending on submitter type
 UK_Parliament.submitterRouting = function () {
 
@@ -53,8 +60,6 @@ UK_Parliament.submitterRouting = function () {
 };
 
 UK_Parliament.submitterRouting();
-
-UK_Parliament.submitterIdentification();
 
 // Function to listen for checked radio buttons and change form action
 UK_Parliament.radioRouting = function () {
@@ -225,3 +230,110 @@ UK_Parliament.formValidation = function () {
 };
 
 UK_Parliament.formValidation();
+
+// Dropdown switch for hiding/showing content using form elements
+UK_Parliament.dropdownSwitch = function () {
+
+  if (document.querySelector('[data-dropdown="switch"]')) {
+
+    // Grab all elements which act as content switches
+    var dropdownSwitcherList = document.querySelectorAll('[data-dropdown="switch"]');
+
+    // Grab all elements which act as content content blocks
+    var dropdownContentList = document.querySelectorAll('[data-dropdown-content]');
+
+    // Find closest parent element function
+    var closestParentElement = function (el, fn) {
+      return el && (fn(el) ? el : closestParentElement(el.parentElement, fn));
+    };
+
+    // Show hidden content blocks function
+    var contentShow = function (dc) {
+      dc.style.display = 'block';
+    };
+
+    // Hide visible content blocks function
+    var contentHide = function (dc) {
+      dc.style.display = 'none';
+    };
+
+    // Set default content visibility state
+    dropdownContentList.forEach(function (elements) {
+
+      // Grab visibility state (show/hide) of each content block
+      var dropdownContentInitalState = elements.getAttribute('data-dropdown-content');
+
+      if (dropdownContentInitalState === 'show') {
+        contentShow(elements);
+      } else if (dropdownContentInitalState === 'hide') {
+        contentHide(elements);
+      }
+    });
+
+    // Loop through content switches
+    dropdownSwitcherList.forEach(function (elements) {
+
+      // Grab the switch/content holder using closestElement function
+      var dropdownHolder = closestParentElement(elements, function (el) {
+        return el.querySelector('[data-dropdown-content]');
+      });
+
+      // Grab the content block associated with holder and switch
+      var dropdownContent = dropdownHolder.querySelector('[data-dropdown-content]');
+
+      // Store reference to content data attribute
+      var dropdownContentState = dropdownContent.getAttribute('data-dropdown-content');
+
+      // Handle checkbox switches
+      if (elements.type === 'checkbox') {
+        elements.addEventListener('click', function () {
+          if (this.checked) {
+            if (dropdownContentState === 'show') {
+              contentHide(dropdownContent);
+            } else if (dropdownContentState === 'hide') {
+              contentShow(dropdownContent);
+            }
+          } else {
+            if (dropdownContentState === 'show') {
+              contentShow(dropdownContent);
+            } else if (dropdownContentState === 'hide') {
+              contentHide(dropdownContent);
+            }
+          }
+        });
+      }
+
+      // Handle radio group switches
+      if (elements.type === 'radio') {
+
+        // Grab radio group name
+        var inputSwitchName = elements.name;
+
+        // Grab radio group elements with that name
+        var inputSwitchNamesArray = document.getElementsByName(inputSwitchName);
+
+        // Loop radio group and grab clicked radio button
+        inputSwitchNamesArray.forEach(function (radios, index) {
+          radios.addEventListener('click', function () {
+
+            if (this.getAttribute('data-dropdown') === 'switch' && this.checked) {
+              if (dropdownContentState === 'show') {
+                contentHide(dropdownContent);
+              } else if (dropdownContentState === 'hide') {
+                contentShow(dropdownContent);
+              }
+            } else {
+              if (dropdownContentState === 'show') {
+                contentShow(dropdownContent);
+              } else if (dropdownContentState === 'hide') {
+                contentHide(dropdownContent);
+              }
+            }
+          });
+        });
+      }
+    });
+  }
+};
+
+UK_Parliament.dropdownSwitch();
